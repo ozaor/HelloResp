9ff4b527f2269cd33c795cfcb897f28d8c43b189
ozaor <ozaor@amctech.pl>
2017-11-13 13:41:16 +0100

    RepHello
    Files Added

---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CalculatorTests
+{    
+    [TestClass]
+    public class CalculatorTestSuite
+    {
+        HelloNew.Calculator calc;
+         
+        [TestInitialize]
+        public void TestInit()
+        {
+            calc = new HelloNew.Calculator();
+        }
+
+
+        [TestMethod]
+        public void testValidDiv()
+        {
+            double x = 100;
+            double y = 200;
+            double div = calc.division(x, y);
+            Assert.AreEqual(0.5, div);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void testDivByZero()
+        {
+            double x = 100;
+            double y = 0;
+            double div = calc.division(x, y);            
+        }
+    }
+}
---
+++ HelloNew/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNew
+{
+    public class Calculator
+    {
+        public double division(double x, double y)
+        {
+       
+            if (y == 0)
+            {
+                throw new System.DivideByZeroException("Can not divide by 0");
+            }
+            else
+                return x / y;
+        }
+    }
+}
