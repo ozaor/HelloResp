bdba4a07f923726e15d64ac1de6174d71e85956b
wczapla <wczapla@amctech.pl>
2018-01-11 13:52:45 +0100

    baranina

---
+++ wczaplatest/test1/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,20 @@
+﻿namespace NUnit.Tests1
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.AreEqual(2, 3, "nie przeszlo");
+        }
+    }
+}
